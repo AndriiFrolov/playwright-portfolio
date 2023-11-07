a96e46a71345af0e154739d06c73d2498edca118
olexandr1234 <olexandrgordeev@gmail.com>
2023-11-07 12:46:29 +0200

    test

--- framework/pages/profile/ProfilePage.ts
+++ framework/pages/profile/ProfilePage.ts
@@ -1,4 +1,4 @@
-import test, { Locator, Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { BasePage } from "framework/pages/BasePage";
 import { BookSummaryFragment } from "../bookstore/BookSummaryFragment";
 import { BookRow } from "./BookRow";
@@ -8,17 +8,17 @@ export class ProfilePage extends BasePage {
   //locators
   readonly logOutBtn: Locator = this.page.getByRole("button", { name: "Log out" });
   readonly goToBookStoreBtn: Locator = this.page.locator("id=gotoStore");
-  readonly deleteConfirmBtn: Locator = this.page.locator("//button[@id='closeSmallModal-ok']");
-  
+  readonly deleteConfirmBtn: Locator = this.page.locator("//button[@id='closeSmallModal-ok']"); 
+
 
   //fragments
   public getBookRow(index: number): BookSummaryFragment {
     return new BookSummaryFragment(this.page, index);
   }
+
   public getBookLink(title: string): BookRow {
     return new BookRow(this.page, title);
   }
-
   //methods
   pageUrl(): string {
     return "/profile";
